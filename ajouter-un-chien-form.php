95114c987558000e9aef599f0254788fcf607677
Anjuide <anjuide@users.noreply.github.com>
2019-02-13 08:18:47 +0100

    fonction upload

--- ajouter-un-chien-form.php
+++ ajouter-un-chien-form.php
@@ -1,17 +1,20 @@
 <?php
 // SE CONNECTER A LA BASE DE DONNEES
 require("classes/Connexion.php");
+include("classes/functions.php");
 // DEMARRER UNE NOUVEL OBJET DE CONNEXION
 $appli = new Connexion();
 // RECUPERER LES POSTS
 $nickname = $_POST['nickname'];
-$date = $_POST['date'];
-$photo = $_POST['photo'];
+$birthday = $_POST['birthday'];
 $race1 = $_POST['race1'];
 $race2 = $_POST['race2'];
 $userId = 2;
 
+$picture = upload($_FILES['picture']);
+
 // APPEL DE MA FONCTION QUI AJOUTE UN CHIEN
-$appli->setDog($nickname, $date, $photo, $userId);
+$appli->setDog($nickname, $birthday, $picture, $userId);
+header("Location: index.php");
 
 ?>
