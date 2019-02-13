cc634759122b78752181209176df8352f6afb553
Anjuide <anjuide@users.noreply.github.com>
2019-02-13 08:20:26 +0100

    fonction date et update articleId

--- article-du-chien-form.php
+++ article-du-chien-form.php
@@ -5,10 +5,10 @@ require("classes/Connexion.php");
 $appli = new Connexion();
 // RECUPERER LES POSTS
 $comment = $_POST['comment'];
-$publicationDate = "2019-01-08 05:21:24";
-$articleId = $_GET["id"];
+// $publicationDate = "2019-01-08 05:21:24";
+$articleId = $_POST['idArticle'];
 $userId = 2;
-
+$publicationDate = date('Y-m-d H:i:s');
 // APPEL DE MA FONCTION QUI AJOUTE UN ARTICLE
 $appli->setComment ($comment, $publicationDate, $articleId, $userId);
 header("Location: article-du-chien?id=$articleId.php#comments");
