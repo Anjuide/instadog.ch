3513cc30288254997bab9b2700013308873d2064
Anjuide <anjuide@users.noreply.github.com>
2019-02-13 08:19:54 +0100

    date => birthday + photo => picture

--- ajouter-un-chien.php
+++ ajouter-un-chien.php
@@ -8,18 +8,18 @@ include "header.php"
             <div class="card card-signin my-5">
                 <div class="card-body">
                     <h5 class="card-title text-center">Ajouter Un Chien</h5>
-                    <form class="form-signin" action="ajouter-un-chien-form.php" method="POST">
+                    <form class="form-signin" action="ajouter-un-chien-form.php" method="POST" enctype="multipart/form-data" >
                         <div class="form-label-group">
                             <label for="inputPrenom">Nom du chien</label>
                             <input type="text" id="inputPrenom" name ="nickname" class="form-control" placeholder="NickeNom" required autofocus> 
                         </div>
                         <div class="form-label-group">
                             <label for="inputNom"></label>
-                            <input type="Date" id="inputNom" name ="date" class="form-control" placeholder="Date" required> 
+                            <input type="Date" id="inputNom" name ="birthday" class="form-control" placeholder="Date" required> 
                         </div>
                         <div class="form-label-group">
                             <label for="inputPays">Téléverser la photo de votre chien</label>
-                            <input type="file" id="inputPhoto" name ="photo" class="form-control" placeholder="Téléverser la photo de votre chien" required> 
+                            <input type="file" id="inputPhoto" name ="picture" class="form-control" placeholder="Téléverser la photo de votre chien" required> 
                         </div>
    
                         <div class="input-group mb-3">
